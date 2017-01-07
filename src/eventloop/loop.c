10bb52ecc958f558b1d39bc4a383eb894e7d4a09
Christian Grabowski <christiang11754@gmail.com>
2017-01-07 18:21:11 -0500

    add event loop

---
+++ src/eventloop/loop.c
@@ -0,0 +1,21 @@
+#include <stdlib.h>
+#include "loop.h"
+
+uv_loop_t* new_loop()
+{
+    uv_loop_t *loop = malloc(sizeof(uv_loop_t));
+    uv_loop_init(loop);
+    return loop;
+}
+
+void run_events(uv_loop_t *loop)
+{
+    uv_run(loop, UV_RUN_DEFAULT);
+}
+
+void destroy_loop(uv_loop_t *loop)
+{
+    uv_loop_close(loop);
+    free(loop);
+}
+
