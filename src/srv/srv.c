7b657640f8d2066c798c7b09e00c12e21e3b92b4
Christian Grabowski <christiang11754@gmail.com>
2017-01-07 22:27:17 -0500

    add serve and send functions

--- src/srv/srv.c
+++ src/srv/srv.c
@@ -1,27 +1,63 @@
 #include <stdlib.h>
 #include <stdio.h>
 #include <sys/socket.h>
+#include <netinet/ip.h>
 #include "srv.h"
 
-int get_socket(char *proto)
+static void on_recv(uv_udp_t *handler, ssize_t nread, const uv_buf_t *rcvbuf, const struct sockaddr *addr, unsigned flags)
 {
-    case "UDP":
-    case "udp":
-        return socket(AF_INET, SOCK_DGRAM, 17);
-    default:
-        printf("unsupported protocol requested");
-        return -1;
+    if(nread > 0)
+    {
+        printf("received msg");
+    }
+    free(rcvbuf->base);
 }
 
-int listen_and_serve(struct Server *s)
+static void on_alloc(uv_handle_t *client, size_t suggest_size, uv_buf_t *buf)
 {
-    // TODO implement listen_and_serve once libuv loop is implemented
+    buf->base = malloc(suggested_size);
+    buf->len = suggested_size;
 }
 
-struct Server* new_server(char* proto, char* addr)
+int listen_and_serve(server_t *s, uv_loop_t *loop)
 {
-    struct Server* s = (struct Server*)calloc(4, sizeof(struct Server));
+    uv_udp_init(loop, s->socket);
+    struct sockaddr_in srv_addr;
+    uv_ip4_addr(s->addr_str, s->port, s->socket);
+    int status = uv_udp_bind(s->socket, (const struct sockaddr*)&srv_addr, UV_UDP_REUSE_ADDR);
+    if(status != 0)
+    {
+        printf("could not bind to addr");
+        return status;
+    }
+    s->addr = &srv_addr;
+    status = uv_udp_recv_start(s->socket, on_alloc, on_recv);
+    if(status != 0)
+    {
+        printf("could not open for receiving packets");
+        return status;
+    }
+    return 0;
+}
+
+static void on_send(uv_udp_send_t *req, int status)
+{
+    printf("sent");
+}
+
+int send_msg(server_t *s, char* payload)
+{
+    uv_udp_send_t req;
+    uv_buf_t buf = buf_init(payload, sizeof(payload));
+    return uv_udp_send(&req, s->socket, &buf, s->addr, on_send);
+}
+
+server_t* new_server(char* addr, int port)
+{
+    uv_udp_t *socket = (uv_udp_t*)malloc(sizeof(uv_udp_t))
+    server_t* s = (server_t*)malloc(sizeof(server_t));
     s->addr_str = addr;
-    s->socket = get_socket(proto);
+    s->port = port;
+    s->socket = socket;
     return s;
 }
