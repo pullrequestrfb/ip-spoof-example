34220c21b5fa7c3a00e9faa0a038ba6343b6957e
Christian Grabowski <christiang11754@gmail.com>
2017-01-10 17:22:17 -0500

    if using an OS socket, have libuv access it

--- src/ip_spoof.c
+++ src/ip_spoof.c
@@ -3,10 +3,11 @@
 #include <uv.h>
 #include "eventloop/loop.h"
 #include "srv/srv.h"
+#include "spoof/socket.h"
 
 int main() {
     uv_loop_t *loop = new_loop();
-    server_t *server = new_server("0.0.0.0", 8080);
+    server_t *server = new_server("0.0.0.0", 8080, get_socket());
     listen_and_serve(server, loop);
     return 0;
 }
