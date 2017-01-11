6351b522dd8dd5efae7582814709e0f93694291d
Christian Grabowski <christiang11754@gmail.com>
2017-01-11 18:02:30 -0500

    WIP signal handling

--- src/eventloop/loop.h
+++ src/eventloop/loop.h
@@ -3,6 +3,8 @@
 #ifndef LOOP_H_
 #define LOOP_H_
     uv_loop_t* new_loop();
+
     void run_events(uv_loop_t *loop);
+
     void destroy_loop(uv_loop_t *loop);
 #endif
--- src/ip_spoof.c
+++ src/ip_spoof.c
@@ -1,14 +1,30 @@
 #include <stdlib.h>
 #include <stdio.h>
+#include <unistd.h>
+#include <signal.h>
 #include <uv.h>
 #include "eventloop/loop.h"
 #include "srv/srv.h"
 #include "spoof/socket.h"
 
+uv_loop_t *loop;
+
+void handle_signal(int sig)
+{
+    if(loop && (sig == SIGINT || sig == SIGTERM))
+    {
+        destroy_loop(loop);
+    }
+}
+
 int main() {
-    uv_loop_t *loop = new_loop();
+    printf("starting server on 0.0.0.0:8080");
+    signal(SIGINT, &handle_signal);
+    signal(SIGTERM, &handle_signal);
+    loop = new_loop();
     int socket_fd = get_socket();
     server_t *server = new_server("0.0.0.0", 8080, socket_fd);
     listen_and_serve(server, loop);
+    run_events(loop);
     return 0;
 }
